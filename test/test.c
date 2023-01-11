1615d1b12a9bfd4a99662b96ccaa5a5bcf8936ef
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-01-11 21:07:29 +0700

    add option to check if using clang for `test_c` and `test_cpp` targets

--- test/test.c
+++ test/test.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 
 int main() {
+#if defined(CHECK_USING_CLANG) && !defined(__clang__)
+  printf("compiler is not clang\n");
+  return 1;
+#endif
   printf("all ok\n");
   return 0;
 }
