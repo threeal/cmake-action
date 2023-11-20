a83bb9b6ec7f1b3145c047287c62cd06e98a0e7d
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-11-20 17:14:25 +0700

    build: initialize JavaScript action project

---
+++ main/index.mjs
@@ -0,0 +1,8 @@
+/******/ /* webpack/runtime/compat */
+/******/ 
+/******/ if (typeof __nccwpck_require__ !== 'undefined') __nccwpck_require__.ab = new URL('.', import.meta.url).pathname.slice(import.meta.url.match(/^file:\/\/\/\w:/) ? 1 : 0, -1) + "/";
+/******/ 
+/************************************************************************/
+var __webpack_exports__ = {};
+
+//# sourceMappingURL=main.mjs.map
