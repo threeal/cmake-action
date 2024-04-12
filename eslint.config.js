2f79fde5e74f7fb1c017adb0a3a47d72e5f7caa2
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-04-12 18:34:26 +0700

    test: add TypeScript support to new flat ESLint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,10 @@
 import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
 
 export default [
   eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...tseslint.configs.stylistic,
   {
     ignores: [".*", "dist"],
   },
