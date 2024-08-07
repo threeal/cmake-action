67186870360782bf4e4346f5cafd7765ef831b11
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-08-07 19:41:58 +0700

    build: utilize Rollup to bundle file

---
+++ rollup.config.js
@@ -0,0 +1,10 @@
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import typescript from "@rollup/plugin-typescript";
+
+export default {
+  input: "src/index.ts",
+  output: {
+    dir: "dist",
+  },
+  plugins: [nodeResolve(), typescript()],
+};
