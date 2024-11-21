d91c238854456dd8356f8a2205e0d57ca9129aeb
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-11-21 22:04:45 +0700

    feat: add `exec` function
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

---
+++ src/exec.ts
@@ -0,0 +1,26 @@
+import { spawn } from "node:child_process";
+
+/**
+ * Executes a command with the given arguments.
+ *
+ * The command is executed with `stdin` ignored and both `stdout` and `stderr` inherited by the parent process.
+ *
+ * @param command The command to execute.
+ * @param args The arguments to pass to the command.
+ * @returns A promise that resolves when the command exits successfully or rejects if it exits with a non-zero status code or encounters an error.
+ */
+export async function exec(command: string, args: string[]): Promise<void> {
+  return new Promise<void>((resolve, reject) => {
+    const proc = spawn(command, args, {
+      stdio: ["ignore", "inherit", "inherit"],
+    });
+    proc.on("error", reject);
+    proc.on("close", (code) => {
+      if (code === 0) {
+        resolve();
+      } else {
+        reject(new Error(`Command exited with status code ${code}`));
+      }
+    });
+  });
+}
