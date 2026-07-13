fc3cd12631f9591db44efeca30854f20bd170593
Alfi Maulana <alfi.maulana.f@gmail.com>
2026-07-13 22:46:32 +0700

    remove `all` option in `vitest.config.ts`
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     coverage: {
-      all: false,
       enabled: true,
       reporter: ["text"],
       thresholds: { 100: true },
